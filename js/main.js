0cc5fadea47aa324032bef254a2b48b019d1fe7e
Pavel <pl.hancharou@gmail.com>
2020-10-08 12:31:08 +0300

    create/connect  main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const money = '';
+const income = '';
+const addExpenses = '';
+const deposit = '';
+const mission = '';
+const period = '';
+
+alert('hello world!');
+console.log('object');
