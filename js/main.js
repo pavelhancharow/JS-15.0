194dbbff87035826f05623005fcb7c8aa2f233f8
Pavel <pl.hancharou@gmail.com>
2020-10-09 15:30:03 +0300

    assign values

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const money = '';
-const income = '';
-const addExpenses = '';
-const deposit = '';
-const mission = '';
-const period = '';
+const money = 500;
+const income = '250';
+const addExpenses = '20, 300, 30';
+const deposit = true;
+const mission = 1500;
+const period = 12;
 
-alert('hello world!');
-console.log('object');
+// alert('hello world!');
+// console.log('object');
