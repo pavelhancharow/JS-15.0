edd2c0725d2b9bc72de96ee1b09342c2339235e4
Pavel <pl.hancharou@gmail.com>
2020-10-09 16:01:08 +0300

    Обязательно задание

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@
 
 const money = 500;
 const income = '250';
-const addExpenses = '20, 300, 30';
+const addExpenses = 'Internet, Food, Pay for housing payments';
 const deposit = true;
 const mission = 1500;
 const period = 12;
 
-// alert('hello world!');
-// console.log('object');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев' + '\nЦель заработать ' + mission + ' долларов');
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
